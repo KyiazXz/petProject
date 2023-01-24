770a65005cae518bbbcc432c499330623680f925
KYIAZBEK ZHOLDOSHEV <111270349+KyiazXz@users.noreply.github.com>
2023-01-24 10:59:26 +0300

    done

---
+++ petProjects/src/projects/todoList/Todo.jsx
@@ -0,0 +1,16 @@
+// import React from 'react'
+// import { useState } from 'react'
+// import TodoForm from './TodoForm'
+// export default function Todo() {
+//     const [edit , setEdit] = useState ('')
+//   return (
+//     <>
+//     {todos.map(( el ,index)=> {
+//         return (<div key={index}>
+//             <div key={index}> 
+//             {el}</div>
+//         </div>)
+//     })}
+//     </>
+//   )
+// }
---
+++ petProjects/src/projects/todoList/TodoList.jsx
@@ -0,0 +1,18 @@
+// import React from 'react'
+// import { useState } from 'react'
+// import TodoForm from './TodoForm'
+// export default function TodoList() {
+//     const [todos , setTodos] = useState ([]);
+
+
+//     const addTodos = (todo) => {
+//         const newTodos = [todo , ...todos]
+//         setTodos (newTodos)
+//     }
+//   return (
+//     <>
+//     <TodoForm />
+//     </>
+
+//   )
+// }
